3b9e6fe1ce30ce037a01080a43ae9d59e89297be
Nissim Ram <nissimram1812@gmail.com>
2022-02-28 20:53:45 -0500

    deploying w express server

--- index.js
+++ index.js
@@ -114,3 +114,11 @@ async function automateThankYous(){
 }
 
 
+import express from "express"
+const app = express();
+
+
+app.use(express.json({ extended: false }));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
