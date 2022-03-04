90105921f51b83629d0235879715ccf0ce884acf
Nissim Ram <nissimram1812@gmail.com>
2022-03-04 16:46:31 -0500

    deploy test 5

--- api/index.js
+++ api/index.js
@@ -8,5 +8,8 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 app.get('/test', function(req, res){
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
     res.json({status: 'message received'})
+    res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
 })
