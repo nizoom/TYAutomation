803ea52867cafffa9dce0103c6a97f27e8914bc5
Nissim Ram <nissimram1812@gmail.com>
2022-03-07 14:42:47 -0500

    removing port for heroku

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ app.use("/index", (req, res) => {
 
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 
 // initNodeMailer([{templateName : 'newdonor'}])
