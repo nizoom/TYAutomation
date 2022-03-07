0408a5be869984a4ab39d569613af9306b3f4707
Nissim Ram <nissimram1812@gmail.com>
2022-03-07 14:54:50 -0500

    trying deploy with app.get

--- index.js
+++ index.js
@@ -28,9 +28,20 @@ import express from "express";
 
 const app = express();
 
-app.use(express.json({ extended: false }));
 
-// app.use("/api/endpoint", endpoint);
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
+
+  
 
 app.use("/index", (req, res) => {
     try {
