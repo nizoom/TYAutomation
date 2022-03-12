cb42c37390ccfc62e9315ba93fd20ceec861369e
Nissim Ram <nissimram1812@gmail.com>
2022-03-12 17:25:56 -0500

    env environment available to assess loginjs

--- templatebuilderfuncs/assesslogin.js
+++ templatebuilderfuncs/assesslogin.js
@@ -2,9 +2,10 @@ import "dotenv/config.js";
 
 function assessLoginAttempt(attemptedPW){
 
-    const correctPW = process.env.APP_PW
-
+    const correctPW = process.env.UI_APP_PW
+ 
     console.log(correctPW);
+    console.log(process.env);
 
     if(correctPW === attemptedPW){
         console.log('success')
