0fd3dcc013328ebe8258d5116628616728b2b3d5
Nissim Ram <nissimram1812@gmail.com>
2022-03-14 18:54:55 -0400

    adding local port

--- index.js
+++ index.js
@@ -101,8 +101,8 @@ app.use("/index", async function(req, res) {
 
 });
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
 // async function automateThankYous(){
 
