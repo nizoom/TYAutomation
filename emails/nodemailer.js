d9283dfb15aab5918dabb2a8d5172026f7f8762a
Nissim Ram <nissimram1812@gmail.com>
2022-03-15 12:56:03 -0400

    testing email endpoint

--- emails/nodemailer.js
+++ emails/nodemailer.js
@@ -52,7 +52,7 @@ async function createNewEmail(donation){
     let mailOptions = {
 
       from: 'cohen@commonthreadsproject.org', //ThankYouFromCTP@outlook.com' ALL TESTING USE THIS EMAIL 
-      to: '', //donation.TYToEmailAddress,//['info@commonthreadsproject.org', donation.TYToEmailAddress],  //['cohen@commonthreadsproject.org'],//'cohen@commonthreadsproject.org'], 
+      to: donation.TYToEmailAddress,//['info@commonthreadsproject.org', donation.TYToEmailAddress],  //['cohen@commonthreadsproject.org'],//'cohen@commonthreadsproject.org'], 
       bcc : ['nissimram1812@gmail.com'],//, 'info@commonthreadsproject.org'],
       subject: donation.emailSubject,
       text: '',
