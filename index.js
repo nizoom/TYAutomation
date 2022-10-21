e8deac191e3be71d86bc78d355776772eebd3f3f
Nissim Ram <nissimram1812@gmail.com>
2022-10-20 21:49:45 -0400

    removed extra call to init program

--- index.js
+++ index.js
@@ -146,7 +146,7 @@ app.use("/index", async function (req, res) {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
 
-automateThankYous();
+// automateThankYous();
 
 async function automateThankYous() {
   console.log("Beginning automation process");
