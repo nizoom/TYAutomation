3547a6ef2195c5f5ad747086e694bca17c36ba1c
nizoom <nissimram1812@gmail.com>
2024-05-23 21:55:31 -0400

    tested writing to file with fake data

--- utils/addemailrecord.js
+++ utils/addemailrecord.js
@@ -5,21 +5,32 @@ import { generateDonationRecordID } from "./recordutlls.js";
 class Record {
   constructor(id, emailStatus) {
     this.id = id;
-    tjos.emailStatus = emailStatus;
+    this.emailStatus = emailStatus;
   }
 }
 
 const addRecord = async (name, timestamp, emailStatus) => {
   try {
-    let currentFile = [];
     if (fs.existsSync(emailRecordsFilePath)) {
       const fileContent = await fs.promises.readFile(
         emailRecordsFilePath,
         "utf-8"
       );
-      currentFile = JSON.parse(fileContent);
+      const currentFileData = JSON.parse(fileContent);
       const recordId = generateDonationRecordID(name, timestamp);
       const newRecord = new Record(recordId, emailStatus);
+      const updatedFileData = [...currentFileData, newRecord];
+
+      await fs.promises.writeFile(
+        emailRecordsFilePath,
+        JSON.stringify(updatedFileData, null, 2),
+        "utf-8"
+      );
+      console.log("record recorded successfully");
     }
-  } catch (error) {}
+  } catch (error) {
+    console.error("Error writing to file", error);
+  }
 };
+
+addRecord("John-Smith", "2024-01-01", "SENT");
