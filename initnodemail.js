fb0b0fff4935d7b6e196874431fed259fe8a1448
nizoom <nissimram1812@gmail.com>
2024-10-04 16:25:56 -0400

    revert to previous node mailer function

---
+++ initnodemail.js
@@ -0,0 +1,31 @@
+import createNewEmail from "./emails/nodemailer.js";
+
+async function initNodeMailer(donations, sendResponseFromNodeMailerToClient) {
+  console.log(donations.length);
+  //loop through donations and pass each obj to nodemailer
+
+  const mailResults = await Promise.all(
+    donations.map(async (donation) => {
+      //set timeout to avoid error with too many node mailer calls back to back
+
+      const index = donations.indexOf(donation);
+
+      return new Promise((resolve) => {
+        setTimeout(async () => {
+          const emailStatus = await createNewEmail(
+            donation,
+            sendResponseFromNodeMailerToClient
+          ); //maybe add .thens to it
+
+          resolve(emailStatus);
+        }, 1000 * index);
+      });
+    })
+  );
+
+  //store results
+
+  return mailResults;
+}
+
+export default initNodeMailer;
